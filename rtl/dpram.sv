663e17b20543f0715a174c0747892ab1ecbc811a
Daniel Garbi <danielrgarbi@gmail.com>
2024-04-10 22:12:44 -0300

    Add reset synchronizer in dpram

--- rtl/dpram.sv
+++ rtl/dpram.sv
@@ -22,7 +22,19 @@ module dpram #(
     input  [PtrWidth-1:0] i_rd_ptr,
     output [   Width-1:0] o_rd_data
 );
-  reg [Width-1:0] ram[Depth];
+  logic [Width-1:0] ram[Depth];
+  logic rst_sync0_n;
+  logic rst_sync1_n;
+
+  always_ff @(posedge clk_rd or negedge rst_n) begin
+    if (!rst_n) begin
+        rst_sync0_n <= 1'b0;
+        rst_sync1_n <= 1'b0;
+    end else begin
+        rst_sync0_n <= rst_n;
+        rst_sync1_n <= rst_sync0_n;
+    end
+  end
 
   always_ff @(posedge clk_wr) begin
     if (i_wr_en && ~i_wr_full) begin
@@ -30,8 +42,8 @@ module dpram #(
     end
   end
 
-  always_ff @(posedge clk_rd or negedge rst_n) begin
-    if (!rst_n) begin
+  always_ff @(posedge clk_rd or negedge rst_sync1_n) begin
+    if (!rst_sync1_n) begin
       o_rd_data <= '0;
     end else if (i_rd_en && ~i_rd_empty) begin
       o_rd_data <= ram[i_wr_ptr];
